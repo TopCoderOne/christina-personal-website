e819474a7368b7d15ef882f681093b40b6a29dc6
TopCoderOne <227909025+TopCoderOne@users.noreply.github.com>
2025-10-16 14:47:43 +0300

    commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// NavToggle and Nav
+const navToggle = document.querySelector(".nav-toggle");
+const nav = document.querySelector(".nav");
+
+navToggle.addEventListener("click", () => {
+    navToggle.classList.toggle("opened");
+    nav.classList.toggle("opened");
+});
+
+nav.addEventListener("click", (e) => {
+    if (e.target.tagName === 'A') {
+        navToggle.classList.remove("opened");
+        nav.classList.remove("opened");
+    }
+});
+
+// Accordions
+const accordions = document.getElementById("accordions");
+
+accordions.addEventListener("click", (e) => {
+    let item = e.target.closest(".accordion");
+    if (item.classList.contains("accordion")) {
+        if (item.classList.contains("active")) {
+            item.classList.remove("active");
+        } else {
+            accordions.querySelectorAll(".accordion").forEach(el => {
+                el.classList.remove("active");
+            });
+            item.classList.add("active");
+        }
+    }
+});
